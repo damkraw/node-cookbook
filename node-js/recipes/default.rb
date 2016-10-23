b1241b75482fa56e0aa14fc6fb2c5af7da06273e
Damian Krawczyk <damian@frequency.com>
2016-10-23 23:37:19 +0200

    different strategy

--- node-js/recipes/default.rb
+++ node-js/recipes/default.rb
@@ -1,5 +1,16 @@
-#
-# Cookbook Name:: node-js
-# Recipe:: default
-#
-# Copyright (c) 2016 The Authors, All Rights Reserved.
+execute "what am i" do
+time = Time.now.to_i
+  cwd '/tmp'
+  command "mkdir -p setup_#{time}"
+  action :run
+end
+
+execute "upgrade to node 5.1.0" do
+   command "/usr/local/bin/npm cache clean -f && /usr/local/bin/npm install -g n && /usr/local/bin/n 6.9.1"
+   action :run
+end
+
+execute "restart node service via monit" do
+   command "/usr/bin/monit restart all"
+   action :run
+end
